b62c065dfe56791574ac4155965e377c3352021d
Lunacea <linnuan.01@gmail.com>
2025-05-22 00:46:08 +0900

    docs: add JSDoc in NavLink

--- src/app/components/NavLink.tsx
+++ src/app/components/NavLink.tsx
@@ -1,7 +1,17 @@
 import Link from "next/link";
 
-export default function NavLink({ href, children }: { href: string, children: React.ReactNode }) {
+/**
+ * NavLink コンポーネント
+ * props（properties）は、コンポーネントに渡されるデータです。
+ * @param href - リンクのhref。属性値は原則そのまま命名します。
+ * @param children - リンクの子要素。タグで囲んだ内容を渡します。
+ * @returns NavLink コンポーネント
+ */
+export default function NavLink(
+    { href, children }: { href: string, children: React.ReactNode }) {
     return (
+        // クラスを指定するときは、classNameを使用します。（Reactのルール）
+        // tailwindcssでは、CSSプロパティをクラス名に変換して指定します。
         <Link href={href} className="mr-4 bg-amber-50 text-blue-600">
             {children}
         </Link>
